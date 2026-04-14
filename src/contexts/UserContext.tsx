edb54f778df15fe53b8b29e971c104baa75dfb45
git-mahfuj <mahfujurrheeck@gmail.com>
2026-04-14 14:01:26 +0600

    feat: implement real-time profile contexts

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { User } from "lucide-react";
+import React, { createContext, useContext, useState } from "react";
+
+interface UserType {
+  name?: string;
+  email?: string;
+  bio?: string;
+  image?: {
+    file: File;
+    url: string;
+  };
+  links?:[
+    
+  ]
+}
+type UserContextType = {
+  user: UserType | undefined;
+  setUser: React.Dispatch<React.SetStateAction<UserType | undefined>>;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined,
+);
+
+export const UserContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [user, setUser] = useState<UserType | undefined>(undefined);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUser must be used within a UserContextProvider");
+  }
+  return context;
+};
